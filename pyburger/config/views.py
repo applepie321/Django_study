c4969028fe911e8f8a8ccc7fe33338e831d81d58
applepie321 <apple00551155@gmail.com>
2023-04-03 22:26:08 +0900

    0403
    0403 Django

--- pyburger/config/views.py
+++ pyburger/config/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
+from burgers.models import Burger
 
 def main(request):
     return render(request, "main.html")
 
 def burger_list(request):
-    return render(request, "burger_list.html")
+    burgers = Burger.objects.all()
+    print("Entire burger list: ", burgers)
+    context = {
+        "burgers": burgers,
+    }    
+    return render(request, "burger_list.html", context)
+
+def burger_search(request):
+    return render(request, "burger_search.html")
+
+    
