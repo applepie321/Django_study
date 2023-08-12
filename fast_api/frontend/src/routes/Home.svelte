4ad2bce30d80b614fc5f3c80787f5a0f35c3ef92
applepie321 <112625800+applepie321@users.noreply.github.com>
2023-08-12 22:47:04 +0900

    0812

--- fast_api/frontend/src/routes/Home.svelte
+++ fast_api/frontend/src/routes/Home.svelte
@@ -1,7 +1,7 @@
 <script>
     import fastapi from "../lib/api";
     import { link } from 'svelte-spa-router'
-    import { page } from "../lib/store"
+    import { page, is_login } from "../lib/store"
     import moment from 'moment/min/moment-with-locales'
     moment.locale('ko')
 
@@ -28,9 +28,10 @@
 <div class="container my-3">
     <table class="table">
         <thead>
-        <tr class="table-dark">
+        <tr class="text-center table-dark">
             <th>번호</th>
-            <th>제목</th>
+            <th style="width: 50%;">제목</th>
+            <th>글쓴이</th>
             <th>작성일시</th>
         </tr>
         </thead>
@@ -70,5 +71,6 @@
             </li>
         </ul>
         <!-- 페이징처리 끝 -->
-    <a use:link href="/question-create" class="btn btn-primary">질문 등록하기</a>
+    <a use:link href="/question-create" 
+        class="btn btn-primary {$is_login ? '' : 'disabled'}">질문 등록하기</a>
 </div>
